d76a52e3285c3b3d65920fd7550fedf3620f00e8
SupahFox <31490939+SupahFox@users.noreply.github.com>
2023-09-04 01:21:28 -0300

    comm

--- Ejercicio 1/src/Library/Shelve.cs
+++ Ejercicio 1/src/Library/Shelve.cs
@@ -4,15 +4,24 @@ namespace Ucu.Poo.Expert
 {
     public class ShelveBook
     {
+        //Cargo la clase Book
         public Book Book { get; }
+
+        //Cargo los atributos de la clase ShelveBook
         public string LibrarySector { get ; set; }
+
+        //Cargo los atributos de la clase LibraryShelve
         public string LibraryShelve { get ; set; }
+
+        //Creo el constructor de la clase ShelveBook
         public ShelveBook(Book book, String librarySector, String libraryShelve)
         {
             this.Book = book;
             this.LibrarySector = librarySector;
             this.LibraryShelve = libraryShelve;
         }
+
+        //Creo el método GetInfo para obtener la información de los libros en Program.cs
         public void GetInfo()
         {
             Console.WriteLine($"{this.Book.Title} del autor {this.Book.Author} código: {this.Book.Code}\nSe encuentra en el sector {this.LibrarySector} de la biblioteca estantería {this.LibraryShelve}\n");
--- Ejercicio 1/src/Program/Program.cs
+++ Ejercicio 1/src/Program/Program.cs
@@ -7,12 +7,15 @@ namespace Ucu.Poo.Expert
     {
         static void Main()
         {
+            //Creo los libros
             Book book1 = new Book("Design Patterns","Erich Gamma & Others","001-034");
             Book book2 = new Book("Pro C#","Troelsen","001-035");
 
+            //Creo los estantes
             ShelveBook book1_sh = new ShelveBook(book1, "A", "7");
             ShelveBook book2_sh = new ShelveBook(book2, "B", "3");
 
+            //Obtengo la información de los libros
             book1_sh.GetInfo();
             book2_sh.GetInfo();
 
